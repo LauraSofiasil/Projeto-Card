fa703ca13282e7cf7f412849db1a4d0a22483a79
LAURA SOFIA LIMA NUNES DA SILVA <24122467@CFP127ALUNO.LOCAL>
2025-02-11 16:44:47 -0300

    finalizado

--- app.js
+++ app.js
@@ -1,19 +1,20 @@
 'use strict'
 
 const cidades = [
-    {nome: "Jandira", cor: 'yellow', icon: 'jandira.png'},
-    {nome: "Barueri", cor: 'purple', icon: 'barueri.png'},
-    {nome: "Osasco", cor: 'red', icon: 'osasco.png'},
-    {nome: "Itapevi", cor: 'aqua', icon: 'itapevi.png'}
+    {nome: "Marvel", cor: 'red', icon: 'marvel.png'},
+    {nome: "Barbie", cor: 'pink', icon: 'barbie.png'},
+    {nome: "DC", cor: 'yellow', icon: 'dc.png'},
+    {nome: "Disney", cor: 'aqua', icon: 'disney.png'}
 ]
 
 const blocos = [
-    {nome: "Jandira", cor: 'gray', icon: 'mcdonalds.png', descricao: 'Com dois andares, o primeiro McDonalds de Jandira representa um dos modernos projetos tecnológicos da rede e uma diversificação maior de serviços, o que atende de forma mais completa as necessidades dos clientes.'},
-    {nome: "Itapevi", cor: 'gray', icon: 'parque.jpg', descricao: 'O primeiro parque municipal da história de Itapevi está localizado ao lado do Estádio Municipal, numa área com 110 mil m². O local oferece pista para caminhada e ciclovia, bicicletário, academia ao ar livre, playground e praças de skate e patins.'},
-    {nome: "Barueri", cor: 'gray', icon: 'tambore.webp', descricao: 'Em um espaço com mais de 52 mil m² de área construída, seus visitantes encontram um completo mix de lojas, marcas renomadas, serviços de qualidade e diferentes opções gastronômicas e de lazer para todas as idades.'},
-    {nome: "Osasco", cor: 'gray', icon: 'calcadao.jpg', descricao: 'Se você precisa ir às compras sem gastar muito, o melhor lugar é o Calçadão. O local lembra a rua 25 de Março, famoso centro de comércio popular paulistano, mas em menor proporção. '}
+    {nome: "Escola de Princesas", icon: 'escola de princesas.jpg', descricao: 'Blair, uma garota simples e órfã que foi sorteada para estudar na exclusiva Escola de Princesas.', preco: 'R$24,90', estrelas: '5'},
+    {nome: "Princesa das Pérolas", icon: 'perolas.jpg', descricao: 'Lumina, uma sereia. Ela tem um poder mágico capaz de fazer as pérolas brilharem e dançarem.', preco: 'R$29,90', estrelas: '4'},
+    {nome: "A princesa e a popstar", icon: 'popstar.jpg', descricao: 'Tori, a bondosa princesa de Meribella e sua pop star preferida Keira descobrem um segredo mágico.', preco: 'R$29,90', estrelas: '4'},
+    {nome: "O natal perfeito", icon: 'natal.jpg', descricao: 'Barbie e suas irmãs estavam prontas para ir à Nova York para o Natal, mas uma grande nevasca aparece!', preco: 'R$24,90', estrelas: '3'},
 ]
 
+//Criação do menu no header
 function criarMenu(cidade) {
     const novoItem = document.createElement('li')
     const novoLink = document.createElement('a')
@@ -34,28 +35,44 @@ function criarMenu(cidade) {
 
 }
 
-function criarMain(blocos){
+
+//Criação dos cards
+function criarCards(blocos){
     const novoBloco = document.createElement('div') 
     const novaImagem = document.createElement('img')
     const novoTitulo = document.createElement('h1')
     const novoTexto = document.createElement('p')
-    const quadrados = document.getElementById('quadrados')
-
+    const novoPreco = document.createElement('h3')
+    const avaliacao = document.createElement('div')
+    const novoBotao = document.createElement('button')
+    
+    novoBloco.classList.add('novoBloco')
+  
     novaImagem.src = `./img/${blocos.icon}`
 
     novoTitulo.textContent = blocos.nome
 
     novoTexto.textContent = blocos.descricao
+    
+    novoPreco.textContent = blocos.preco
+
+    //Define que o máximo de estrelas que aparecerão será cinco, serão preenchidas as estrelas que correspondem a quantidade que está definida no 'blocos'
+    avaliacao.innerHTML = (`${'★'.repeat(blocos.estrelas)}${'☆'.repeat(5 - blocos.estrelas)}`)
+    
+    novoBotao.textContent = 'Compre aqui'
 
     novoBloco.appendChild(novaImagem)
     novoBloco.appendChild(novoTitulo)
     novoBloco.appendChild(novoTexto)
+    novoBloco.appendChild(novoPreco)
+    novoBloco.appendChild(avaliacao)
+    novoBloco.appendChild(novoBotao)
 
     quadrados.appendChild(novoBloco)
 
 }
 
-blocos.forEach(criarMain)
+blocos.forEach(criarCards)
 
 
 cidades.forEach(criarMenu)
